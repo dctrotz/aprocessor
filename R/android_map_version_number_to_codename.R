9227b2ad43c174f6f70e1c5cc0ad4b026ea83902
David Trotz <davidtrotz@olivetree.com>
2018-03-23 12:46:25 -0700

    Oops, argument not renamed properly.

--- R/android_map_version_number_to_codename.R
+++ R/android_map_version_number_to_codename.R
@@ -11,29 +11,29 @@
 #' [1] "kitkat"
 
 android_map_version_number_to_codename <- function(version_string) {
-    if(str_detect(version_number, "^[^0-9]*1\\.6(\\.[0-9])?")) {
+    if(str_detect(version_string, "^[^0-9]*1\\.6(\\.[0-9])?")) {
         "donut"
-    } else if(str_detect(version_number, "^[^0-9]*2\\.[0-1](\\.[0-9])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*2\\.[0-1](\\.[0-9])?")) {
         "eclair"
-    } else if(str_detect(version_number, "^[^0-9]*2\\.2(\\.[0-3])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*2\\.2(\\.[0-3])?")) {
         "froyo"
-    } else if(str_detect(version_number, "^[^0-9]*2\\.3(\\.[0-7])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*2\\.3(\\.[0-7])?")) {
         "gingerbread"
-    } else if(str_detect(version_number, "^[^0-9]*3\\.[0-2](\\.[0-6])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*3\\.[0-2](\\.[0-6])?")) {
         "honeycomb"
-    } else if(str_detect(version_number, "^[^0-9]*4\\.0(\\.[0-5])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*4\\.0(\\.[0-5])?")) {
         "ice cream sandwich"
-    } else if(str_detect(version_number, "^[^0-9]*4\\.[1-3](\\.[0-1])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*4\\.[1-3](\\.[0-1])?")) {
         "jellybean"
-    } else if(str_detect(version_number, "^[^0-9]*4\\.4(\\.[0-4])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*4\\.4(\\.[0-4])?")) {
         "kitkat"
-    } else if(str_detect(version_number, "^[^0-9]*5\\.[0-1](\\.[0-1])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*5\\.[0-1](\\.[0-1])?")) {
         "lollipop"
-    } else if(str_detect(version_number, "^[^0-9]*6\\.0(\\.[0-1])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*6\\.0(\\.[0-1])?")) {
         "marshmallow"
-    } else if(str_detect(version_number, "^[^0-9]*7\\.[0-1](\\.[0-2])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*7\\.[0-1](\\.[0-2])?")) {
         "nougat"
-    } else if(str_detect(version_number, "^[^0-9]*8\\.[0-1](\\.[0-9])?")) {
+    } else if(str_detect(version_string, "^[^0-9]*8\\.[0-1](\\.[0-9])?")) {
         "oreo"
     } else {
         "unknown"
